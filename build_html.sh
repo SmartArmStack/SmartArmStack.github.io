bbbaef54cebd725df479f0463a63f3addaf31ce2
Murilo Marinho <murilomarinho@manchester.ac.uk>
2025-06-23 11:46:51 +0100

    Add workflow to build html

--- build_html.sh
+++ build_html.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# While 2.0 is not stable
-python -m pip install jupyter-book --pre
-
-# Otherwise the links will not be correctly set up for th webpage
+python -m pip install mystmd
 export BASE_URL="https://smartarmstack.github.io"
-
-# Build page
-python -m jupyter book build --html
+myst build --html
 
