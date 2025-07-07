611135d454d54050402a335535e651a5dc2d3823
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-07 16:16:11 +0100

    [ur_template] Adding videos of the template.

--- docs/scripts/pre_build.sh
+++ docs/scripts/pre_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+# This script should be run in its own folder.
 
 clone_and_rename_readme(){
 # $1 must be the repository name
@@ -14,10 +15,19 @@ cd ..
 }
 
 # Submodules
-cd .. # This script is meant to be called in its own directory
+cd .. # Go to docs
 mkdir -p submodules
 cd submodules
 
 clone_and_rename_readme https://github.com/SmartArmStack/docker-coppeliasim.git main
 clone_and_rename_readme https://github.com/MarinhoLab/sas_ur_control_template.git main
 clone_and_rename_readme https://github.com/MarinhoLab/sas_kuka_control_template.git main
+
+cd .. # Get out of submodules
+cd scripts # Go back to `scripts`
+
+# Download videos from github
+cd .. # Go do docs
+
+curl -Lo submodules/sas_ur_control_template/sas_urct_simulation.mp4 https://github.com/user-attachments/assets/bfee1148-bfe3-4425-80da-04fcd65d2b18
+curl -Lo submodules/sas_ur_control_template/sas_urct_realrobot.mp4 https://github.com/user-attachments/assets/62ac7ccd-d7c8-41f7-8af8-1b17919d90f2
