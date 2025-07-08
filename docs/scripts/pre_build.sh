b86ef4c42f2f239417f98c1419cd97057f9dabe2
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-08 21:36:44 +0100

    Adding videos and update information for sas_kuka_control_template

--- docs/scripts/pre_build.sh
+++ docs/scripts/pre_build.sh
@@ -29,5 +29,9 @@ cd scripts # Go back to `scripts`
 # Download videos from github
 cd .. # Go do docs
 
+# sas_ur_control_template
 curl -Lo submodules/sas_ur_control_template/sas_urct_simulation.mp4 https://github.com/user-attachments/assets/bfee1148-bfe3-4425-80da-04fcd65d2b18
 curl -Lo submodules/sas_ur_control_template/sas_urct_realrobot.mp4 https://github.com/user-attachments/assets/62ac7ccd-d7c8-41f7-8af8-1b17919d90f2
+# sas_kuka_control_template
+curl -Lo submodules/sas_kuka_control_template/sas_kct_simulation.mp4 https://github.com/user-attachments/assets/d0c00b59-8b45-4c11-9c85-6ec9807eda97
+curl -Lo submodules/sas_kuka_control_template/sas_kct_realrobot.mp4 https://github.com/user-attachments/assets/8340a929-487e-4ed7-b256-809f769bc446
