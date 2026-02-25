8ef5f30e1be95a7fa178337c7de41722468754b1
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2026-02-25 22:46:38 +0000

    [template] Try adding b1z1 template to website.

--- docs/scripts/pre_build.sh
+++ docs/scripts/pre_build.sh
@@ -15,7 +15,7 @@ cd "$FOLDER_NAME"
 FILE_NAME="${FOLDER_NAME}"_README.md
 mv README.md FILE_NAME
 # Add author info if needed
-if [[ $3 ]]
+if [[ $3 ]]; then
   sed -i $(printf "---\n author: ${3}\n ---") FILE_NAME
 fi
 
